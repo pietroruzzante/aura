9856bf90f4840b07a2f6ca8eda56982cdfb17ec4
annaghiotto <ghiotto.anna@gmail.com>
2024-04-20 17:31:04 +0200

    headScore updated

--- lib/models/headache_score.dart
+++ lib/models/headache_score.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class HeadacheScore extends ChangeNotifier {
-  int score = 0;
+  double score = 0;
 
-  int calculateScore(int stressScore, int weatherScore){
-    int score = stressScore + weatherScore;
+  double calculateScore(double stressScore, double weatherScore){
+    score = stressScore + weatherScore;
     notifyListeners();
     return score;
   }
