51dfc846270b6ebb497341f7753196a31517b317
ptrrzznt <162702634+ptrrzznt@users.noreply.github.com>
2024-04-21 19:21:55 +0200

    usless classes deleted

--- lib/models/headache_score.dart
+++ lib/models/headache_score.dart
@@ -7,10 +7,11 @@ class HeadacheScore extends ChangeNotifier {
   List<double> scores = [0, 0, 0, 0, 0, 0, 0];
 
   double getScore(int day) {
+    refreshScore();
     return scores[day];
   } //getScore
 
-  void refreshScore(List<double> stressScore, List<double> weatherScore) {
+  void refreshScore() {
     final stressScore = getStress();
     final weatherScore = getWeather();
     for (int i = 0; i < 6; i++) {
