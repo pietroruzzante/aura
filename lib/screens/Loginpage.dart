a6797915e3eddfb7e4edc6b552f96b99ed2e4acf
LeoBadon <leonardobadon21@gmail.com>
2024-05-03 19:31:07 +0200

    login_page_update

--- lib/screens/Loginpage.dart
+++ lib/screens/Loginpage.dart
@@ -3,7 +3,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:stress/models/authentication_service.dart';
 import 'package:stress/screens/Homepage.dart';
 
-
 class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
@@ -14,7 +13,7 @@ class _LoginPageState extends State<LoginPage> {
   final TextEditingController _passwordController = TextEditingController();
   bool _isLoading = false;
   String _errorMessage = '';
-
+  bool _passwordVisible = false;
 
   void _login() async {
     setState(() {
@@ -39,7 +38,7 @@ class _LoginPageState extends State<LoginPage> {
     }
   }
 
-@override
+  @override
   Widget build(BuildContext context) {
     return Container(
       decoration: const BoxDecoration(
@@ -47,8 +46,8 @@ class _LoginPageState extends State<LoginPage> {
           begin: Alignment.topRight,
           end: Alignment.bottomLeft,
           colors: [
-            Colors.blue,
-            Colors.purpleAccent,
+            Color.fromRGBO(186, 233, 255, 1),
+            Color.fromARGB(255, 0, 83, 151),
           ],
         ),
       ),
@@ -60,43 +59,40 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   Widget _page() {
-  return Center(
-    child: SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(32.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            _icon(),
-            const SizedBox(height: 50),
-            _inputField("Email", _emailController),
-            const SizedBox(height: 20),
-            _inputField("Password", _passwordController, isPassword: true),
-            const SizedBox(height: 10),
-            _errorMessage.isNotEmpty
-                ? Text(
-                    _errorMessage,
-                    style: TextStyle(color: Colors.red),
-                  )
-                : SizedBox(), // Show error message if not empty
-            const SizedBox(height: 40),
-            _isLoading
-                ? CircularProgressIndicator()
-                : _loginBtn(),
-          ],
+    return Center(
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _icon(),
+              const SizedBox(height: 50),
+              _inputField("Email", _emailController),
+              const SizedBox(height: 20),
+              _inputField("Password", _passwordController, isPassword: true),
+              const SizedBox(height: 10),
+              _errorMessage.isNotEmpty
+                  ? Text(
+                      _errorMessage,
+                      style: const TextStyle(color: Colors.red),
+                    )
+                  : SizedBox(), // Show error message if not empty
+              const SizedBox(height: 40),
+              _isLoading ? CircularProgressIndicator() : _loginBtn(),
+            ],
+          ),
         ),
       ),
-    ),
-  );
-}
-
+    );
+  }
 
   Widget _icon() {
     return Image.asset(
-              'assets/new_logo.png',
-              height: 250,
-              width: 250,
-            );
+      'assets/new_logo.png',
+      height: 250,
+      width: 250,
+    );
   }
 
   Widget _inputField(String hintText, TextEditingController controller,
@@ -114,9 +110,26 @@ class _LoginPageState extends State<LoginPage> {
           labelStyle: TextStyle(color: Colors.white, fontSize: 15),
           floatingLabelBehavior: FloatingLabelBehavior.auto,
           border: InputBorder.none,
-          contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          contentPadding:
+              const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          prefixIcon: hintText == "Email"
+              ? const Icon(Icons.email, color: Colors.white)
+              : const Icon(Icons.lock, color: Colors.white),
+          suffixIcon: isPassword
+              ? IconButton(
+                  icon: Icon(
+                    _passwordVisible ? Icons.visibility : Icons.visibility_off,
+                    color: Colors.white.withOpacity(0.5),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _passwordVisible = !_passwordVisible;
+                    });
+                  },
+                )
+              : null,
         ),
-        obscureText: isPassword,
+        obscureText: isPassword ? !_passwordVisible : false,
       ),
     );
   }
@@ -130,12 +143,19 @@ class _LoginPageState extends State<LoginPage> {
       ),
       child: TextButton(
         onPressed: _login,
-        style: TextButton.styleFrom(
-          padding: const EdgeInsets.symmetric(vertical: 16),
-        ),
         child: const Text(
           "Login",
-          style: TextStyle(fontSize: 20, color: Colors.blue),
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.blue,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        style: TextButton.styleFrom(
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+          ),
         ),
       ),
     );
