7fc3959c15348f49a740f04cc1f4eff0053f0e1b
ptrrzznt <162702634+ptrrzznt@users.noreply.github.com>
2024-05-14 10:16:46 +0200

    coordinates url

--- lib/services/openWeather.dart
+++ lib/services/openWeather.dart
@@ -3,21 +3,46 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class Openweather{
-  static const APIkey = '9eb79248952ff7bdd77ab6c7ea84ee2f';
-  static const OpenWeatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?zip=35137,IT&appid=';
+  static const APIkey = '1e52585354b785f5d9f0959bcbe3ddb1';
+  static const dataUrl = 'http://api.openweathermap.org/data/2.5/forecast?';
+  static const historicalUrl = 'http://api.openweathermap.org/data/2.5/history/city?';
+  static const coordinatesUrl = 'http://api.openweathermap.org/geo/1.0/zip?zip=';
+  static const ITUrl = ',IT&appid=';
 
-
-  //api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
   Future<Map<String, dynamic>> getData() async {
 
-      const url = OpenWeatherUrl + APIkey;
+    final coordinates = await getCoordinatesUrl(35137); 
+    final url = dataUrl+coordinates+ITUrl+APIkey;
 
-      print('Calling: $url');
-      final response = await http.get(Uri.parse(url));
-      final Map<String, dynamic> decodedResponse = jsonDecode(response.body);
-      return decodedResponse;
+    print('Calling: $url');
+    final response = await http.get(Uri.parse(url));
+    final Map<String, dynamic> decodedResponse = jsonDecode(response.body);
+    return decodedResponse;
   
+  }
+
+  Future<Map<String, dynamic>> getHistoricalData() async {
+    final coordinates = await getCoordinatesUrl(35137);
+    final url = historicalUrl+coordinates+ITUrl+APIkey;
+
+    print('Calling: $url');
+    final response = await http.get(Uri.parse(url));
+    final Map<String, dynamic> decodedResponse = jsonDecode(response.body);
+    return decodedResponse;
+  
+  }
+
+  Future<String> getCoordinatesUrl(int zipCode) async{
+    final zip = zipCode.toString();
+    final url = coordinatesUrl+zip+ITUrl+APIkey;
+    
+    final response = await http.get(Uri.parse(url));
+    final decoded = jsonDecode(response.body);
+    final lat = decoded['lat'];
+    final lon = decoded['lon'];
+    final coordinates = 'lat=${lat.toString()}&lon=${lon.toString()}';
+    return coordinates;
   }
 }
 
