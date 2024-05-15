a60412ab28ea513a07b792cca04c6c37a289d6a2
ptrrzznt <162702634+ptrrzznt@users.noreply.github.com>
2024-05-15 12:04:24 +0200

    Weather API request completed

--- lib/models/headache_score.dart
+++ lib/models/headache_score.dart
@@ -2,9 +2,10 @@ import 'package:aura/services/openWeather.dart';
 import 'package:ml_dataframe/ml_dataframe.dart';
 import 'package:ml_algo/ml_algo.dart';
 import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HeadacheScore {
-  final List<double> _scores = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+  final List<double> _scores = List<double>.filled(7, 0.0);
 
   double operator [](int index) => _scores[index];
 
@@ -16,6 +17,8 @@ class HeadacheScore {
   Future<HeadacheScore> refreshScore() async {
     final stressScore = await getStress();
     final weatherScore = await getWeather();
+    print('weatherScore:${weatherScore}');
+    print('stressScore:${stressScore}');
     for (int i = 0; i < 6; i++) {
       _scores[i] = stressScore[i] + weatherScore[i];
     }
@@ -28,12 +31,12 @@ class HeadacheScore {
     final data = DataFrame([
       featureNames,
       [8.0, 80.0],
-      [7.0, 110.0],
+      [7.0, 90.0],
       [6.0, 70.0],
-      [6.0, 150.0],
-      [6.0, 200.0],
-      [4.0, 100.0],
-      [1.0, 200.0]
+      [6.0, 90.0],
+      [6.0, 80.0],
+      [8.0, 75.0],
+      [7.0, 90.0]
     ]); //Here we need data request from Impact and from database
     final json = await rootBundle.loadString('assets/stress_model.json');
     final classifier = DecisionTreeClassifier.fromJson(json);
@@ -42,32 +45,51 @@ class HeadacheScore {
   } //getStress
 
   Future<List<double>> getWeather() async {
-    //await Future.delayed(
-        //const Duration(seconds: 2)); //Provvisorio fino a accesso server
-    final decodedResponse = await Openweather().getData();
-    final pressures = [];
+    final pressures = List<double>.filled(7, 0.0, growable: true);
     final dates = unixDates();
+    print('dates: $dates');
 
-    for (int timestamp in dates) {
-      double pressure = getPressureForTimestamp(timestamp, decodedResponse);
-      pressures.add(pressure);
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    int zip = 35137;
+
+    //zip = int.parse(prefs.getString('address')!);
+
+    final coordinates = await Openweather().getCoordinates(zip);
+    final decodedResponse = await Openweather().getData(coordinates);
+    double pressure;
+    
+    for(int i=3; i<7; i++){
+      final timestamp = dates[i];
+      String selected = 'forecast';
+      if(i==3){selected = 'current';}
+
+      pressure = getPressureFromTimestamp(timestamp, decodedResponse, selected);
+      print('i=$i: pressure:$pressure');
+      pressures[i] = pressure;
+
     }
 
-  //Here we need data request from weather API with barometric pressure value and from database
-    final weatherScore = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+    print('pressures:$pressures');
+    final weatherScore = List<double>.filled(7, 0.0);
 
-    for (int i = 0; i < 6; i++) {
+    for (int i = 3; i < 7; i++) {
       final tmp = pressures[i];
-      if (tmp >= 1013.0) {
-        weatherScore[i] = 0.0;
-      } else if ((tmp < 1013) & (tmp >= 1007)) {
+      if (tmp >= 1011.0) {
         weatherScore[i] = 1.0;
-      } else if ((tmp < 1007) & (tmp >= 1005)) {
-        weatherScore[i] = 2.0;
-      } else if ((tmp < 1005) & (tmp >= 1003)) {
+      } else if ((tmp < 1011) & (tmp >= 1009)) {
         weatherScore[i] = 3.0;
-      } else if (tmp < 1003) {
+      } else if ((tmp < 1009) & (tmp >= 1007)) {
+        weatherScore[i] = 2.0;
+      } else if ((tmp < 1007) & (tmp >= 1005)) {
+        weatherScore[i] = 3.0;
+      } else if ((tmp < 1005) & (tmp >= 1003)) {
         weatherScore[i] = 4.0;
+      } else if ((tmp < 1003) & (tmp >= 1001)) {
+        weatherScore[i] = 2.0;
+      } else if ((tmp < 1001) & (tmp >= 997)){
+        weatherScore[i] = 1.0;
+      } else if (tmp < 997) {
+        weatherScore[i] = 0.0;
       }
     }
     return weatherScore;
@@ -75,23 +97,48 @@ class HeadacheScore {
 }
 
 
-double getPressureForTimestamp(int timestamp, Map<String, dynamic> decodedResponse) {
-    final forecasts = decodedResponse['list'];
-    double pressure = 0.0; // Valore di default
-    for (final forecast in forecasts) {
-      final forecastTimestamp = forecast['dt'];
-      if (forecastTimestamp == timestamp) {
-        pressure = forecast['main']['pressure'].toDouble();
-        break; // Esci dal ciclo una volta trovata la previsione per il timestamp desiderato
-      }
+double getPressureFromTimestamp(int timestamp, Map<String, dynamic> decodedResponse, String selected) {
+  double pressure = 0.0;
+
+  if (selected == 'current') {
+    if (decodedResponse['current'] != null && decodedResponse['current']['pressure_mb'] != null) {
+      pressure = decodedResponse['current']['pressure_mb'];
+    } else {
+      print("Error: 'current' or 'pressure_mb' is null in decodedResponse");
     }
-    return pressure;
+  } else {
+    if (decodedResponse['forecast'] != null && decodedResponse['forecast']['forecastday'] != null) {
+      for (var forecastday in decodedResponse['forecast']['forecastday']) {
+        if (forecastday['hour'] != null) {
+          for (var value in forecastday['hour']) {
+            final forecastTimestamp = value['time_epoch'];
+            if (forecastTimestamp == timestamp) {
+              if (value['pressure_mb'] != null) {
+                pressure = value['pressure_mb'].toDouble();
+                print('timestamp found');
+              } else {
+                print("Error: 'pressure_mb' is null for timestamp: $timestamp");
+              }
+              break;
+            }
+          }
+        } else {
+          print("Error: 'hour' is null in 'forecastday'");
+        }
+      }
+    } else {
+      print("Error: 'forecast' or 'forecastday' is null in decodedResponse");
+    }
+  }
+  return pressure;
 }
 
-List<int> unixDates(){
-  List<DateTime> dates = List<DateTime>.filled(6, DateTime.now());
+ 
 
-  for (int i = 0; i < 6; i++) {
+List<int> unixDates(){
+  List<DateTime> dates = List<DateTime>.filled(7, DateTime.now());
+
+  for (int i = 0; i < 7; i++) {
     dates[i] = DateTime.now().subtract(Duration(days: 3)).add(Duration(days: i));
   }
   List<int> unixTimestamps = [];
--- lib/services/openWeather.dart
+++ lib/services/openWeather.dart
@@ -1,49 +1,38 @@
 import 'dart:convert';
-//import 'package:shared_preferences/shared_preferences.dart';
+
 import 'package:http/http.dart' as http;
 
 class Openweather{
-  static const APIkey = '1e52585354b785f5d9f0959bcbe3ddb1';
-  static const dataUrl = 'http://api.openweathermap.org/data/2.5/forecast?';
-  static const historicalUrl = 'http://api.openweathermap.org/data/2.5/history/city?';
-  static const coordinatesUrl = 'http://api.openweathermap.org/geo/1.0/zip?zip=';
-  static const ITUrl = ',IT&appid=';
 
-
-  Future<Map<String, dynamic>> getData() async {
-
-    final coordinates = await getCoordinatesUrl(35137); 
-    final url = dataUrl+coordinates+ITUrl+APIkey;
-
-    print('Calling: $url');
-    final response = await http.get(Uri.parse(url));
-    final Map<String, dynamic> decodedResponse = jsonDecode(response.body);
-    return decodedResponse;
+  static const baseUrl = 'http://api.weatherapi.com/v1';
+  static const forecastUrl = '/forecast.json?';
+  static const APIkey = 'cf35e151f042400d98680305241505';
   
-  }
 
-  Future<Map<String, dynamic>> getHistoricalData() async {
-    final coordinates = await getCoordinatesUrl(35137);
-    final url = historicalUrl+coordinates+ITUrl+APIkey;
-
-    print('Calling: $url');
-    final response = await http.get(Uri.parse(url));
-    final Map<String, dynamic> decodedResponse = jsonDecode(response.body);
-    return decodedResponse;
-  
-  }
-
-  Future<String> getCoordinatesUrl(int zipCode) async{
-    final zip = zipCode.toString();
-    final url = coordinatesUrl+zip+ITUrl+APIkey;
+  Future<Map<String, dynamic>> getData(String coordinates) async {
+    String url;
     
+    url = baseUrl+forecastUrl+'&key='+APIkey+'&q='+coordinates+'&days=4';
+
+    print('Calling: $url');
+    final response = await http.get(Uri.parse(url));
+    final Map<String, dynamic> decodedResponse = jsonDecode(response.body);
+    return decodedResponse;
+  }
+
+  Future<String> getCoordinates(int zipCode) async{
+    const OpenWeatherKey = '9eb79248952ff7bdd77ab6c7ea84ee2f';
+    final zip = zipCode.toString();
+    final url = 'http://api.openweathermap.org/geo/1.0/zip?zip=$zip,IT&appid=$OpenWeatherKey';
+    print('Calling: $url');
     final response = await http.get(Uri.parse(url));
     final decoded = jsonDecode(response.body);
     final lat = decoded['lat'];
     final lon = decoded['lon'];
-    final coordinates = 'lat=${lat.toString()}&lon=${lon.toString()}';
+    final coordinates = '${lat.toString()},${lon.toString()}';
+    print('coordinates: $coordinates');
     return coordinates;
   }
+
+
 }
-
-
