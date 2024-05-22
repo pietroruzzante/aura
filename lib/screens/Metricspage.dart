5b97de1fcb750d591704773d2b653df10aabe03f
ptrrzznt <162702634+ptrrzznt@users.noreply.github.com>
2024-05-22 20:00:27 +0200

    circular indicator added in Metricspage

--- lib/screens/Metricspage.dart
+++ lib/screens/Metricspage.dart
@@ -6,7 +6,6 @@ import 'package:aura/services/impact.dart';
 import 'package:dashed_circular_progress_bar/dashed_circular_progress_bar.dart';
 
 class Metricspage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -27,61 +26,127 @@ class Metricspage extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
                       Container(
-                        height: 250,
+                        height: 300,
+                        width: 400,
                         child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                               children: [
-                                Text('Aura score insights',
-                                    style:
-                                        WorkSans.titleMedium.copyWith(color: Palette.white)),
-                                Text('stress score: ${data[1][3]} '),
-                                Text('stress score: ${data[2][3]}'),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Text('Aura score insights',
+                                        style: WorkSans.titleMedium
+                                            .copyWith(color: Palette.white)),
+                                  ],
+                                ),
+                                SizedBox(height: 100),
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        Column(
+                                          children: [
+                                            Text(
+                                                'stress',
+                                                style: WorkSans.titleSmall
+                                                .copyWith(color: Palette.deepBlue)),
+                                            DashedCircularProgressBar(
+                                              width: 100,
+                                              height: 100,
+                                              progress: data[1][3],
+                                              maxProgress: 4,
+                                              startAngle: 225,
+                                              sweepAngle: 270,
+                                              foregroundColor: Palette.deepBlue,
+                                              backgroundColor: Palette.softBlue1,
+                                              foregroundStrokeWidth: 10,
+                                              backgroundStrokeWidth: 10,
+                                              seekColor: Palette.white,
+                                              seekSize: 8,
+                                              animation: true,
+                                              animationDuration:
+                                                  Duration(milliseconds: 500),
+                                              animationCurve:
+                                                  Easing.standardDecelerate,
+                                              child: Center(child: Text('${data[1][3].toInt()}')),
+                                            )
+                                          ],
+                                        ),
+                                        SizedBox(width: 10,),
+                                        Column(
+                                          children: [
+                                            Text(
+                                                'weather',
+                                                style: WorkSans.titleSmall
+                                                .copyWith(color: Palette.deepBlue)),
+                                            DashedCircularProgressBar(
+                                              width: 100,
+                                              height: 100,
+                                              startAngle: 225,
+                                              sweepAngle: 270,
+                                              progress: data[2][3],
+                                              maxProgress: 4,
+                                              foregroundColor: Palette.deepBlue,
+                                              backgroundColor: Palette.softBlue1,
+                                              foregroundStrokeWidth: 10,
+                                              backgroundStrokeWidth: 10,
+                                              seekColor: Palette.white,
+                                              seekSize: 8,
+                                              animation: true,
+                                              animationDuration:
+                                                  Duration(milliseconds: 500),
+                                              animationCurve:
+                                                  Easing.standardDecelerate,
+                                              child: Center(child: Text('${data[2][3].toInt()}')),
+                                            ),
+                                            
+                                          ],
+                                        )
+                                      ],
+                                    )
+                                  ],
+                                )
                               ],
                             ),
                           ],
                         ),
                       ),
-                      SizedBox(height: 50,),
                       Container(
-                        height: 90,
+                        height: 130,
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.start,
                           children: [
                             Row(
                               mainAxisAlignment: MainAxisAlignment.start,
                               children: [
-                                Text('Exercise',
-                                    style:
-                                        WorkSans.titleSmall),
+                                Text('Exercise', style: WorkSans.titleSmall),
                               ],
                             ),
-                            SizedBox(height: 20,),
                             Row(
                               mainAxisAlignment: MainAxisAlignment.start,
                               children: [
                                 Text('Last Date of Exercise: ${data[3]}',
-                                    style: WorkSans.headlineSmall.copyWith(fontSize: 16)),
+                                    style: WorkSans.headlineSmall
+                                        .copyWith(fontSize: 16)),
                               ],
                             )
                           ],
                         ),
                       ),
                       Container(
-                        height: 90,
+                        height: 70,
                         child: Column(
                           children: [
                             Row(
                               mainAxisAlignment: MainAxisAlignment.start,
                               children: [
-                                Text('Sleep',
-                                    style:
-                                        WorkSans.titleSmall),
+                                Text('Sleep', style: WorkSans.titleSmall),
                               ],
-                            ),
-                            SizedBox(
-                              height: 20,
                             ),
                             SleepIndicator(todaySleep: data[0]),
                           ],
@@ -96,15 +161,13 @@ class Metricspage extends StatelessWidget {
     );
   }
 
-
-    Future<List<dynamic>> loadData() async {
+  Future<List<dynamic>> loadData() async {
     final impact = Impact();
     final todaySleep = await impact.getTodaySleep();
     final todayStress = await HeadacheScore().getStress();
     final todayWeather = await HeadacheScore().getWeather();
     final lastDateExercise = await impact.getLastExerciseDate();
     return [todaySleep, todayStress, todayWeather, lastDateExercise];
-
   }
 }
 
@@ -119,23 +182,23 @@ class SleepIndicator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                  LinearProgressIndicator(
-                    value: (todaySleep!) / 12,
-                    minHeight: 10.0,
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text(
-                        'You slept ${todaySleep.toInt()} hours',
-                        style: WorkSans.headlineSmall,
-                      ),
-                    ],
-                  )
-                ]));
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+          LinearProgressIndicator(
+            value: (todaySleep!) / 12,
+            minHeight: 10.0,
+            borderRadius: BorderRadius.circular(5),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                'You slept ${todaySleep.toInt()} hours',
+                style: WorkSans.headlineSmall,
+              ),
+            ],
+          )
+        ]));
   }
 }
